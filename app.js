b4fef4e328061a691d622a55d972e18e671430cb
Eduardo <ledrc26@gmail.com>
2025-02-24 20:00:49 -0600

    se agrego el metodo para que cada que se agregue un metodo se vea reflejado en la interfas y tambien se agrego el metodo par que se sorte el amigo secreto

--- app.js
+++ app.js
@@ -9,7 +9,38 @@ function agregarAmigo()
     }else 
     {
         amigos.push(cajaTexto.value.trim())
+        actualizarLista();
     }
     cajaTexto.value = "";
 }
 
+function actualizarLista()
+{
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+    for(let i = 0; i < amigos.length; i++)
+    {
+        let amigo = document.createElement('li');
+        amigo.textContent = amigos[i];
+        lista.appendChild(amigo);
+    }
+}
+
+function sortearAmigo()
+{
+    let ponerGanador = document.getElementById('resultado');
+    ponerGanador.innerHTML = "";
+    if (amigos.length === 0)
+    {
+        alert('Primero ingresa a tus amigos');
+    }else
+    {
+        let indice = Math.floor(Math.random() * amigos.length);
+        let ganador = document.createElement('li');
+        ganador.textContent = `El amigo secreto sorteado es: ${amigos[indice]}`;
+        amigos.pop();
+        actualizarLista();
+        ponerGanador.appendChild(ganador);
+
+    }
+}
